e09fc9c7e09b8d42b0b925ea8671f402ab31339d
rajeshjani898-star <rajeshjani898@gmail.com>
2026-03-11 05:26:59 +0530

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SmartScanApp from './SmartScanApp'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SmartScanApp />
-  </React.StrictMode>
-)
